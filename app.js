36f7e8575a1a57f6905e22bdacadbfb761543126
Ivan Daum <ivan.daum@gmail.com>
2017-02-10 15:36:25 +0100

    refactor(project): add better structure

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-var ejs = require('ejs')
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http = require('http');
-var path = require('path');
-var routes = require('./routes/index')
-var app = express();
+const APP_PATH = 'app'
+const PUBLIC_PATH = 'public'
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const ejs = require('ejs')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const http = require('http')
+const path = require('path')
+const routes = require('./routes/index')
+const sassMiddleware = require('node-sass-middleware')
+const app = express()
+app.use('/css', sassMiddleware({
+    src: __dirname + '/'+PUBLIC_PATH+'/css',
+    dest: __dirname + '/'+PUBLIC_PATH+'/css',
+    debug: true,
+    outputStyle: 'compressed'
+}));
+// ------- VIEWS
+app.set('views', path.join(__dirname, APP_PATH + '/views'))
+app.set('view engine', 'ejs')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/', routes);
+// ------- ROUTES & COOKIES
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser());
 
-var server = http.createServer(app);
+// ------- ROUTES
+app.use('/public', express.static(path.join(__dirname, PUBLIC_PATH)));
+app.use('/', routes)
 
+// ------- SERVER CONFIG
+const server = http.createServer(app)
+const port = process.env.PORT || '3000'
 
-var port = process.env.PORT || '3000'
-app.set('port', port);
-server.listen(port);
+app.set('port', port)
+server.listen(port)
