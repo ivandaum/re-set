4131f132481b8199657b54886c8315465da2c4b0
Ivan Daum <ivan.daum@gmail.com>
2017-06-27 10:14:01 +0200

    fix

--- public/js/three/AvatarTHREE.js
+++ public/js/three/AvatarTHREE.js
@@ -10,6 +10,7 @@ class AvatarTHREE {
 
 		var color = lerpColor('#c93c22','#008cec',randFloat(0,100)/100);
 		if (user && user.color) {
+			console.log(user);
 			color = rgbToHex(
 				user.color.r,
 				user.color.g,
@@ -20,6 +21,7 @@ class AvatarTHREE {
 		this.avatar = LOADER.mesh.avatar.clone();
 		this.mesh.add(this.avatar);
 
+		this.avatar.children[0].material = this.avatar.children[0].material.clone();
 		this.avatar.children[0].material.color.set(color);
 
 		return this;
